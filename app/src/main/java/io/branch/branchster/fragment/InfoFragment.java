61cdb53a4bb436bd8fd7e17f3309c77f5202ab3a
Curtis Wang <curtis.wang10@gmail.com>
2023-01-09 18:00:30 -0800

    updated android manifest and build.gradle with branch config to begin integrating SDK

--- app/src/main/java/io/branch/branchster/fragment/InfoFragment.java
+++ app/src/main/java/io/branch/branchster/fragment/InfoFragment.java
@@ -6,7 +6,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
